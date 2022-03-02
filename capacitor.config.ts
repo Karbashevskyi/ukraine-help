2e1207f8d97b2c146abeba6cca27dde093f4199e
Ivan Karbashevskyi <ivankarbashevskyi@gmail.com>
2022-03-03 00:01:28 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ukraine-help',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
